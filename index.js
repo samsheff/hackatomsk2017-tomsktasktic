df47bfdcf94671ffb6b69c7ecdd7790f598ac69b
Sam Sheffres <ssheffres@gmail.com>
2017-11-18 14:54:07 +0700

    Add Endpoint Balance

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ web3 = new Web3(new Web3.providers.HttpProvider(process.env.GETH_ADDRESS || "htt
 const express = require('express')
 const app = express()
 
+app.get('/balance', function(req, res) {
+  var balance = web3.eth.getBalance(req.query.address).then(function(balance) {
+    res.send({balance: balance.toString(10)});
+  });
+});
+
 app.listen(3000, function() {
   console.log('CryptoPoker Backend listening on port 3000!')
 });
