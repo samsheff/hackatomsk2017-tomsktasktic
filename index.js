5eca52f8e10838863a9aa320f31f1f57e158e78a
Sam Sheffres <ssheffres@gmail.com>
2017-11-18 16:50:25 +0700

    Add Table Creation Code, Remove Tabix for now

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var Web3 = require('web3');
 web3 = new Web3(new Web3.providers.HttpProvider(process.env.GETH_ADDRESS || "http://geth:8110"));
 
+var clickhouse = require('@apla/clickhouse');
+ch = new clickhouse(process.env.CLICKHOUSE_ADDRESS || "clickhouse");
+
 const express = require('express')
 const app = express()
+
+const CREATE_DATABASE_STRING = "CREATE DATABASE IF NOT EXISTS cryptopoker;"
+const CREATE_TABLE_STRING = "CREATE TABLE IF NOT EXISTS cryptopoker.accounts (address String, encryptedWallet String, password String, createdAt DateTime DEFAULT now()) Engine = Log;";
 
 app.get('/balance', function(req, res) {
   var balance = web3.eth.getBalance(req.query.address).then(function(balance) {
@@ -18,5 +24,9 @@ app.get('/register', function(req, res) {
 });
 
 app.listen(3000, function() {
-  console.log('CryptoPoker Backend listening on port 3000!')
+  console.log('CryptoPoker Backend listening on port 3000!');
+
+  ch.query(CREATE_DATABASE_STRING, function () {
+    ch.query(CREATE_TABLE_STRING);
+  });
 });
